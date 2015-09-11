eef5e5fdb7b9f9c243524c98ffee0e89ed225482
Maxime Roussin-Bélanger <maxime.roussin-belanger.1@ulaval.ca>
2015-09-10 21:14:43 -0400

    Commiting work to be able to work at home

---
+++ housematch-spring-webapp/src/test/java/ca/ulaval/glo4003/housematch/spring/web/controllers/LoginControllerTest.java
@@ -0,0 +1,63 @@
+package ca.ulaval.glo4003.housematch.spring.web.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.hasProperty;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("file:housematch-spring-webapp/src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
+public class LoginControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void init() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).dispatchOptions(true).build();
+    }
+
+    @Test
+    public void loginControllerShouldRenderLoginView() throws Exception {
+        MockHttpServletRequestBuilder getRequest = get("/login").accept(MediaType.ALL);
+        ResultActions results = mockMvc.perform(getRequest);
+
+        results.andExpect(status().isOk());
+        results.andExpect(view().name("login"));
+    }
+
+    @Test
+    public void loginControllerShouldRenderLoginViewWithUsernameAndPassword() throws Exception {
+        MockHttpServletRequestBuilder getRequest = get("/login").accept(MediaType.ALL);
+        ResultActions results = mockMvc.perform(getRequest);
+
+        results.andExpect(model().attribute("loginFormViewModel", hasProperty("username")));
+        results.andExpect(model().attribute("loginFormViewModel", hasProperty("password")));
+
+    }
+
+    @Test
+    public void whenUserTryToLoginWithGoodCredentialShouldRenderTheHomepage() throws Exception {
+        mockMvc.perform(post("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
+
+}
