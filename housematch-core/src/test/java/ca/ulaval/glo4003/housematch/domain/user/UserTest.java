3927cbb0c2f145c5dd946c3c4d963dbe272a0f87
olivierr91 <o.robert@hotmail.com>
2015-09-13 18:22:51 -0400

    Fixed sentence formulation of test names

--- housematch-core/src/test/java/ca/ulaval/glo4003/housematch/domain/user/UserTest.java
+++ housematch-core/src/test/java/ca/ulaval/glo4003/housematch/domain/user/UserTest.java
@@ -1,40 +1,44 @@
 package ca.ulaval.glo4003.housematch.domain.user;
 
-import org.junit.Before;
-import org.junit.Test;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class UserTest {
 
-    private static final UserRole A_ROLE = UserRole.SELLER;
+    private static final UserRole ROLE = UserRole.SELLER;
+    private static final UserRole ANOTHER_ROLE = UserRole.SELLER;
     private static final String PASSWORD = "PASSWORD1234";
+    private static final String ANOTHER_PASSWORD = "PASSWORD1234";
     private static final String EMAIL = "email@hotmail.com";
+    private static final String ANOTHER_EMAIL = "email2@hotmail.com";
     private static final String USERNAME = "Alice";
+    private static final String CAPITALIZED_USERNAME = "ALICE";
     private static final String ANOTHER_USERNAME = "Bob";
-    private static final String CAPITALIZE_USER = "ALICE";
     private User user;
 
     @Before
     public void setUp() throws Exception {
-        user = new User(USERNAME, EMAIL, PASSWORD, A_ROLE);
+        user = new User(USERNAME, EMAIL, PASSWORD, ROLE);
     }
 
     @Test
-    public void whenComparingTheSameUserItShouldBeTheSameUser() {
-        assertTrue(user.equals(user));
+    public void usersWithTheSameUsernameShouldBeEqual() {
+        User anotherUser = new User(USERNAME, ANOTHER_EMAIL, ANOTHER_PASSWORD, ANOTHER_ROLE);
+        assertTrue(user.equals(anotherUser));
     }
 
     @Test
-    public void whenComparingDifferentUsersItShouldBeDifferentUser() {
-        User anotherUser = new User(ANOTHER_USERNAME, EMAIL, PASSWORD, A_ROLE);
+    public void usersWithDifferentUsernameShouldNotBeEqual() {
+        User anotherUser = new User(ANOTHER_USERNAME, EMAIL, PASSWORD, ROLE);
         assertFalse(user.equals(anotherUser));
     }
 
     @Test
-    public void whenComparingUserWithUsernameDifferentCapitalizationItShouldStillBeTheSameUser() {
-        User anotherUser = new User(CAPITALIZE_USER, EMAIL, PASSWORD, A_ROLE);
+    public void usersHavingDifferentUsernameCapitalizationShouldBeEqual() {
+        User anotherUser = new User(CAPITALIZED_USERNAME, EMAIL, PASSWORD, ROLE);
         assertTrue(user.equals(anotherUser));
     }
 }
