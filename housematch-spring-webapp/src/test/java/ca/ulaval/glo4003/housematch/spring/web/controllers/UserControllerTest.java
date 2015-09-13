c3d005fffe06d916e8fa418bca5589f17e6f80bc
olivierr91 <o.robert@hotmail.com>
2015-09-13 15:09:10 -0400

    Removed unused imports

--- housematch-spring-webapp/src/test/java/ca/ulaval/glo4003/housematch/spring/web/controllers/UserControllerTest.java
+++ housematch-spring-webapp/src/test/java/ca/ulaval/glo4003/housematch/spring/web/controllers/UserControllerTest.java
@@ -6,8 +6,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -16,7 +14,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import ca.ulaval.glo4003.housematch.services.UserService;
