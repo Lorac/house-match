c646de6fa6b870b58b67dd777933e8112054d7a0
VampiricRambo <dkatanari@gmail.com>
2015-09-16 22:58:05 -0400

    DEV: forgot to add back this little fellow after the import hell. (#glo-10)

---
+++ housematch-spring-webapp/src/test/java/ca/ulaval/glo4003/housematch/spring/web/controllers/UserControllerTest.java
@@ -0,0 +1,61 @@
+package ca.ulaval.glo4003.housematch.spring.web.controllers;
+
+import static org.hamcrest.Matchers.hasProperty;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import ca.ulaval.glo4003.housematch.services.UserService;
+
+ class UserControllerTest extends ControllerTest {
+	
+    private UserService userServiceMock;
+    private UserController userController;
+
+    @Before
+    public void init() {
+        super.init();
+        userServiceMock = mock(UserService.class);
+        userController = new UserController(userServiceMock);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(userController).setViewResolvers(viewResolver).build();
+    }
+
+    @Test
+    public void loginControllerShouldRenderLoginView() throws Exception {
+        MockHttpServletRequestBuilder getRequest = get("/login").accept(MediaType.ALL);
+        ResultActions results = mockMvc.perform(getRequest);
+
+        results.andExpect(status().isOk());
+        results.andExpect(view().name("login"));
+    }
+
+    @Test
+    public void loginControllerShouldRenderLoginViewWithUsernameAndPassword() throws Exception {
+        MockHttpServletRequestBuilder getRequest = get("/login").accept(MediaType.ALL);
+        ResultActions results = mockMvc.perform(getRequest);
+
+        results.andExpect(model().attribute("loginForm", hasProperty("username")));
+        results.andExpect(model().attribute("loginForm", hasProperty("password")));
+
+    }
+    
+    @Test
+    public void logoutControllerRendersLoginPageUponLogout() throws Exception {
+    	MockHttpServletRequestBuilder getRequest = get("/logout").accept(MediaType.ALL);
+        ResultActions results = mockMvc.perform(getRequest);
+        
+        results.andExpect(view().name("login"));
+        results.andExpect(status().isOk());
+    }
+    
+}
