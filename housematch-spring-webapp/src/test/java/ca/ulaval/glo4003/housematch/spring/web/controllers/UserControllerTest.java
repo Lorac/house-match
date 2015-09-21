3d07f6adf84a97b905ebfac1d3c5a8557ac55132
VampiricRambo <dkatanari@gmail.com>
2015-09-21 12:17:36 -0400

    TESTING : wrote tests that allowed us to confirm that our views are rendered properly.

--- housematch-spring-webapp/src/test/java/ca/ulaval/glo4003/housematch/spring/web/controllers/UserControllerTest.java
+++ housematch-spring-webapp/src/test/java/ca/ulaval/glo4003/housematch/spring/web/controllers/UserControllerTest.java
@@ -1,25 +1,34 @@
 package ca.ulaval.glo4003.housematch.spring.web.controllers;
 
 import static org.hamcrest.Matchers.hasProperty;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.anyString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.mockito.Mockito.mock;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import ca.ulaval.glo4003.housematch.domain.DomainException;
 import ca.ulaval.glo4003.housematch.services.UserService;
 
 public class UserControllerTest extends ControllerTest {
-	
+    
     private UserService userServiceMock;
     private UserController userController;
+    private String sampleString = "SAMPLE";
+    
+    protected MockHttpSession mockSession;
 
     @Before
     public void init() {
@@ -27,6 +36,7 @@ public class UserControllerTest extends ControllerTest {
         userServiceMock = mock(UserService.class);
         userController = new UserController(userServiceMock);
         this.mockMvc = MockMvcBuilders.standaloneSetup(userController).setViewResolvers(viewResolver).build();
+        mockSession = new MockHttpSession();
     }
 
     @Test
@@ -54,6 +64,30 @@ public class UserControllerTest extends ControllerTest {
         ResultActions results = mockMvc.perform(getRequest);
         
         results.andExpect(view().name("login"));
+        results.andExpect(status().isOk());
+    }
+    
+    @Test
+    public void accessRefusedToRestrictedPagesToUserWithWrongRole() throws Exception {
+        mockSession.setAttribute("username", sampleString);
+        doThrow(new DomainException("")).when(userServiceMock).validateRole(anyString(), anyString());
+        
+        MockHttpServletRequestBuilder getRequest = get("/seller").accept(MediaType.ALL);
+        ResultActions results = mockMvc.perform(getRequest.session(mockSession));
+        
+        results.andExpect(view().name("login"));
+        results.andExpect(status().isOk());
+    }
+    
+    @Test
+    public void accessGrantedToRestrictedPagesToUserWithRightRole() throws Exception {
+        doNothing().when(userServiceMock).validateRole(sampleString, sampleString);
+        mockSession.setAttribute("username", sampleString);
+        
+        MockHttpServletRequestBuilder getRequest = get("/seller").accept(MediaType.ALL);
+        ResultActions results = mockMvc.perform(getRequest.session(mockSession));
+        
+        results.andExpect(view().name("sellerPage"));
         results.andExpect(status().isOk());
     }
     
