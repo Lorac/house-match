bc27da54bcc36762c4437c918a899cd1bc0157fb
Maxime Roussin-Bélanger <maxime.roussinbelanger@gmail.com>
2015-09-25 22:53:01 -0400

    Renamed javaxMailSender

--- housematch-emailer/src/main/java/ca/ulaval/glo4003/housematch/email/EmailSender.java
+++ housematch-emailer/src/main/java/ca/ulaval/glo4003/housematch/email/EmailSender.java
@@ -7,19 +7,19 @@ import javax.mail.internet.MimeMessage;
 public class EmailSender {
 
     private final String ADDRESS_FROM = "housematchb5@gmail.com";
-    private JavaxMailSender emailSender;
+    private JavaxMailSender javaxMailSender;
 
-    public EmailSender(final JavaxMailSender emailSender) {
-        this.emailSender = emailSender;
+    public EmailSender(final JavaxMailSender javaxMailSender) {
+        this.javaxMailSender = javaxMailSender;
     }
 
     public void send(String subject, String content, String email) throws SendEmailException {
         try {
-            emailSender.addRecipient(MimeMessage.RecipientType.TO, email);
-            emailSender.setFrom(new InternetAddress(ADDRESS_FROM));
-            emailSender.setSubject(subject);
-            emailSender.setContent(content);
-            emailSender.send();
+            javaxMailSender.addRecipient(MimeMessage.RecipientType.TO, email);
+            javaxMailSender.setFrom(new InternetAddress(ADDRESS_FROM));
+            javaxMailSender.setSubject(subject);
+            javaxMailSender.setContent(content);
+            javaxMailSender.send();
         } catch (MessagingException e) {
             throw new SendEmailException("Couldn't send the email", e);
         }
