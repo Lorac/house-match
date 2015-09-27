bfbc5d0ff6c1c13bbbd390759540575820afaf95
Olivier Robert <o.robert@hotmail.com>
2015-09-26 21:42:55 -0400

    Removed useless XmlTextEncryptor

--- housematch-xml-persistence/src/main/java/ca/ulaval/glo4003/housematch/domain/user/XmlUserAdapter.java
+++ housematch-xml-persistence/src/main/java/ca/ulaval/glo4003/housematch/domain/user/XmlUserAdapter.java
@@ -2,25 +2,26 @@ package ca.ulaval.glo4003.housematch.domain.user;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-import ca.ulaval.glo4003.housematch.persistence.XmlTextEncryptor;
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.jasypt.util.text.TextEncryptor;
 
 public class XmlUserAdapter extends XmlAdapter<XmlUser, User> {
 
-    private XmlTextEncryptor xmlTextEncryptor;
+    private TextEncryptor textEncryptor;
 
     public XmlUserAdapter() {
-        this(new XmlTextEncryptor());
+        this(new BasicTextEncryptor());
     }
 
-    public XmlUserAdapter(final XmlTextEncryptor xmlTextEncryptor) {
-        this.xmlTextEncryptor = xmlTextEncryptor;
+    public XmlUserAdapter(final TextEncryptor textEncryptor) {
+        this.textEncryptor = textEncryptor;
     }
 
     @Override
     public User unmarshal(XmlUser xmlUser) throws Exception {
         User user = new User();
         user.username = xmlUser.username;
-        user.password = xmlTextEncryptor.decrypt(xmlUser.password);
+        user.password = textEncryptor.decrypt(xmlUser.password);
         user.email = xmlUser.email;
         user.role = xmlUser.role;
         user.activated = xmlUser.activated;
@@ -31,7 +32,7 @@ public class XmlUserAdapter extends XmlAdapter<XmlUser, User> {
     public XmlUser marshal(User user) throws Exception {
         XmlUser xmlUser = new XmlUser();
         xmlUser.username = user.username;
-        xmlUser.password = xmlTextEncryptor.encrypt(user.password);
+        xmlUser.password = textEncryptor.encrypt(user.password);
         xmlUser.email = user.email;
         xmlUser.role = user.role;
         xmlUser.activated = user.activated;
