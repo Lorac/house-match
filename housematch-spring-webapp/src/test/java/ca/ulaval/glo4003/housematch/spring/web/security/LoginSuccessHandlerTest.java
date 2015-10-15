1d959a90d0fd60c15f851fcd25e84df34b5c407e
Olivier Robert <o.robert@hotmail.com>
2015-10-15 01:29:25 -0400

    Unit tests for LoginSuccessHandler et UserAuthenticationProvider

---
+++ housematch-spring-webapp/src/test/java/ca/ulaval/glo4003/housematch/spring/web/security/LoginSuccessHandlerTest.java
@@ -0,0 +1,92 @@
+package ca.ulaval.glo4003.housematch.spring.web.security;
+
+import static org.junit.Assert.assertSame;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.RedirectStrategy;
+
+import ca.ulaval.glo4003.housematch.domain.user.User;
+import ca.ulaval.glo4003.housematch.spring.web.controllers.BaseController;
+
+public class LoginSuccessHandlerTest {
+
+    private RedirectStrategy redirectStrategyMock;
+    private HttpServletRequest httpServletRequestMock;
+    private HttpServletResponse httpServletResponseMock;
+    private HttpSession httpSessionMock;
+    private User userMock;
+    private Authentication authenticationMock;
+
+    private LoginSuccessHandler loginSuccessHandler;
+
+    @Before
+    public void init() {
+        initMocks();
+        stubMethods();
+        loginSuccessHandler = new LoginSuccessHandler();
+        loginSuccessHandler.setRedirectStrategy(redirectStrategyMock);
+    }
+
+    private void initMocks() {
+        redirectStrategyMock = mock(RedirectStrategy.class);
+        httpServletRequestMock = mock(HttpServletRequest.class);
+        httpServletResponseMock = mock(HttpServletResponse.class);
+        httpSessionMock = mock(HttpSession.class);
+        userMock = mock(User.class);
+        authenticationMock = mock(Authentication.class);
+    }
+
+    private void stubMethods() {
+        when(authenticationMock.getPrincipal()).thenReturn(userMock);
+        when(httpServletRequestMock.getSession()).thenReturn(httpSessionMock);
+    }
+
+    @Test
+    public void handlerDoesNotRedirectWhenHttpServletResponseIsCommitedOnLoginSuccess() throws Exception {
+        when(httpServletResponseMock.isCommitted()).thenReturn(true);
+        loginSuccessHandler.handle(httpServletRequestMock, httpServletResponseMock, authenticationMock);
+        verify(redirectStrategyMock, never()).sendRedirect(eq(httpServletRequestMock), eq(httpServletResponseMock),
+                any(String.class));
+    }
+
+    @Test
+    public void userIsAddedToHttpSessionOnLoginSuccess() throws Exception {
+        loginSuccessHandler.handle(httpServletRequestMock, httpServletResponseMock, authenticationMock);
+        verify(httpSessionMock).setAttribute(BaseController.USER_ATTRIBUTE_NAME, userMock);
+    }
+
+    @Test
+    public void handlerRedirectsToEmailReconfirmationUrlWhenuserIsNotActivated() throws Exception {
+        when(userMock.isActivated()).thenReturn(false);
+        loginSuccessHandler.handle(httpServletRequestMock, httpServletResponseMock, authenticationMock);
+        verify(redirectStrategyMock).sendRedirect(httpServletRequestMock, httpServletResponseMock,
+                BaseController.EMAIL_RECONFIRM_URL);
+    }
+
+    @Test
+    public void handlerRedirectsToHomeUrlWhenUserIsActivated() throws Exception {
+        when(userMock.isActivated()).thenReturn(true);
+        loginSuccessHandler.handle(httpServletRequestMock, httpServletResponseMock, authenticationMock);
+        verify(redirectStrategyMock).sendRedirect(httpServletRequestMock, httpServletResponseMock,
+                BaseController.HOME_URL);
+    }
+
+    @Test
+    public void settingRedirectStrategySetsTheSpecifiedRedirectStrategy() {
+        loginSuccessHandler.setRedirectStrategy(redirectStrategyMock);
+        assertSame(redirectStrategyMock, loginSuccessHandler.getRedirectStrategy());
+    }
+
+}
---
+++ housematch-spring-webapp/src/test/java/ca/ulaval/glo4003/housematch/spring/web/security/UserAuthenticationProviderTest.java
@@ -0,0 +1,98 @@
+package ca.ulaval.glo4003.housematch.spring.web.security;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.stream.Stream;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import ca.ulaval.glo4003.housematch.domain.user.User;
+import ca.ulaval.glo4003.housematch.domain.user.UserRole;
+import ca.ulaval.glo4003.housematch.services.user.UserService;
+import ca.ulaval.glo4003.housematch.services.user.UserServiceException;
+
+public class UserAuthenticationProviderTest {
+
+    private static final String SAMPLE_USERNAME = "name";
+    private static final String SAMPLE_PASSWORD = "1234";
+    private static final UserRole SAMPLE_ROLE = UserRole.BUYER;
+
+    private Authentication authenticationMock;
+    private UserService userServiceMock;
+    private User userMock;
+
+    private UserAuthenticationProvider userAuthenticationProvider;
+
+    @Before
+    public void init() throws Exception {
+        initMocks();
+        stubMethods();
+        userAuthenticationProvider = new UserAuthenticationProvider(userServiceMock);
+    }
+
+    private void initMocks() {
+        authenticationMock = mock(Authentication.class);
+        userServiceMock = mock(UserService.class);
+        userMock = mock(User.class);
+    }
+
+    private void stubMethods() throws Exception {
+        when(authenticationMock.getName()).thenReturn(SAMPLE_USERNAME);
+        when(authenticationMock.getCredentials()).thenReturn(SAMPLE_PASSWORD);
+        when(userServiceMock.getUserByLoginCredentials(SAMPLE_USERNAME, SAMPLE_PASSWORD)).thenReturn(userMock);
+        when(userMock.isActivated()).thenReturn(true);
+        when(userMock.getRole()).thenReturn(SAMPLE_ROLE);
+    }
+
+    @Test
+    public void authenticationProviderRetrievesUserByLoginCredentialsFromTheUserService() throws Exception {
+        performAuthentication();
+        verify(userServiceMock).getUserByLoginCredentials(SAMPLE_USERNAME, SAMPLE_PASSWORD);
+    }
+
+    @Test(expected = BadCredentialsException.class)
+    public void authenticationProviderThrowsBadCredentialsExceptionOnUserServiceException() throws Exception {
+        doThrow(new UserServiceException()).when(userServiceMock).getUserByLoginCredentials(SAMPLE_USERNAME,
+                SAMPLE_PASSWORD);
+        performAuthentication();
+    }
+
+    @Test
+    public void authenticationProviderCreatesTheAuthenticationTokenFromTheSpecifiedCredentials() throws Exception {
+        UsernamePasswordAuthenticationToken authenticationToken = performAuthentication();
+
+        assertSame(userMock, (User) authenticationToken.getPrincipal());
+        assertEquals(SAMPLE_PASSWORD, authenticationToken.getCredentials());
+    }
+
+    @Test
+    public void authenticationProviderCreatesTheAuthenticationTokenUsingTheRolesOfTheUser() throws Exception {
+        UsernamePasswordAuthenticationToken authenticationToken = performAuthentication();
+
+        Stream<GrantedAuthority> grantedAuthorityStream = authenticationToken.getAuthorities().stream();
+        assertTrue(grantedAuthorityStream.anyMatch(a -> a.getAuthority().equals(SAMPLE_ROLE.getDisplayName())));
+    }
+
+    @Test
+    public void authenticationProviderCreatesTheAuthenticationTokenWithoutRolesWhenUserIsNotActivated()
+            throws Exception {
+        when(userMock.isActivated()).thenReturn(false);
+        UsernamePasswordAuthenticationToken authenticationToken = performAuthentication();
+        assertTrue(authenticationToken.getAuthorities().isEmpty());
+    }
+
+    private UsernamePasswordAuthenticationToken performAuthentication() {
+        return (UsernamePasswordAuthenticationToken) userAuthenticationProvider.authenticate(authenticationMock);
+    }
+}
