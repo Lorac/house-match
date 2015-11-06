76ff30fe716aebfdf12f4764e31dc6fe9212672e
Maxime Roussin-Bélanger <maxime.roussinbelanger@gmail.com>
2015-11-06 00:12:55 -0500

    Fixed array out of bound error

--- housematch-context/src/main/java/ca/ulaval/glo4003/housematch/context/SpringDemoContext.java
+++ housematch-context/src/main/java/ca/ulaval/glo4003/housematch/context/SpringDemoContext.java
@@ -25,7 +25,7 @@ public class SpringDemoContext extends ContextBase {
     private PropertyRepository propertyRepository;
 
     public SpringDemoContext(final UserFactory userFactory, final UserRepository userRepository, final PropertyFactory propertyFactory,
-            final PropertyRepository propertyRepository) {
+                             final PropertyRepository propertyRepository) {
         this.userFactory = userFactory;
         this.userRepository = userRepository;
         this.propertyFactory = propertyFactory;
@@ -79,15 +79,15 @@ public class SpringDemoContext extends ContextBase {
     private PropertyDetails createRandomPropertyDetails() {
         PropertyDetails propertyDetails = new PropertyDetails();
         Random random = new Random();
-        propertyDetails.setPropertyStyle(PropertyStyle.values()[random.nextInt() % PropertyStyle.values().length]);
-        propertyDetails.setOwnershipType(PropertyOwnershipType.values()[random.nextInt() % PropertyOwnershipType.values().length]);
+        propertyDetails.setPropertyStyle(PropertyStyle.values()[random.nextInt(PropertyStyle.values().length)]);
+        propertyDetails.setOwnershipType(PropertyOwnershipType.values()[random.nextInt(PropertyOwnershipType.values().length)]);
         propertyDetails.setNumberOfExteriorParkingSpaces(random.nextInt(3) + 1);
         propertyDetails.setNumberOfInteriorParkingSpaces(random.nextInt(3));
         propertyDetails.setNumberOfLevels(random.nextInt(3) + 1);
         propertyDetails.setYearOfConstruction(random.ints(50, 1900, 2015).findAny().getAsInt());
 
 
-        propertyDetails.setBackyardDirection(CardinalDirection.values()[random.nextInt() % PropertyOwnershipType.values().length]);
+        propertyDetails.setBackyardDirection(CardinalDirection.values()[random.nextInt(CardinalDirection.values().length)]);
         propertyDetails.setTotalNumberOfRooms(random.nextInt(4) + 3);
         propertyDetails.setNumberOfBedrooms(random.nextInt(4) + 1);
         propertyDetails.setNumberOfBathrooms(random.nextInt(3) + 1);
