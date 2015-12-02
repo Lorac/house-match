a547c892522b0bea5176818a848daaf0c0b8399a
Olivier Robert <o.robert@hotmail.com>
2015-12-01 21:13:21 -0500

    Domain notification tests

---
+++ housematch-core/src/test/java/ca/ulaval/glo4003/housematch/domain/notification/NotificationSettingsTest.java
@@ -0,0 +1,50 @@
+package ca.ulaval.glo4003.housematch.domain.notification;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NotificationSettingsTest {
+
+    private static final NotificationType SAMPLE_NOTIFICATION_TYPE = NotificationType.FAVORITE_PROPERTY_MODIFIED;
+
+    private NotificationSettings notificationSettings;
+
+    @Before
+    public void init() {
+        notificationSettings = new NotificationSettings();
+    }
+
+    @Test
+    public void settingTheNotificationIntervalSetsTheNotificationInterval() {
+        notificationSettings.setNotificationInterval(SAMPLE_NOTIFICATION_TYPE, NotificationInterval.DAILY);
+        assertEquals(NotificationInterval.DAILY, notificationSettings.getNotificationInterval(SAMPLE_NOTIFICATION_TYPE));
+    }
+
+    @Test
+    public void differentNotificationIntervalsAreConsideredAsDifferent() {
+        notificationSettings.setNotificationInterval(SAMPLE_NOTIFICATION_TYPE, NotificationInterval.DAILY);
+        assertFalse(notificationSettings.notificationIntervalEquals(SAMPLE_NOTIFICATION_TYPE, NotificationInterval.WEEKLY));
+    }
+
+    @Test
+    public void theSameNotificationIntervalIsConsideredAsEqual() {
+        notificationSettings.setNotificationInterval(SAMPLE_NOTIFICATION_TYPE, NotificationInterval.DAILY);
+        assertTrue(notificationSettings.notificationIntervalEquals(SAMPLE_NOTIFICATION_TYPE, NotificationInterval.DAILY));
+    }
+
+    @Test
+    public void notificationIsConsideredAsDisabledWhenNotificationIntervalIsSetToNever() {
+        notificationSettings.setNotificationInterval(SAMPLE_NOTIFICATION_TYPE, NotificationInterval.NEVER);
+        assertFalse(notificationSettings.isNotificationTypeEnabled(SAMPLE_NOTIFICATION_TYPE));
+    }
+
+    @Test
+    public void notificationIsConsideredAsEnabledWhenNotificationIntervalIsNotSetToNever() {
+        notificationSettings.setNotificationInterval(SAMPLE_NOTIFICATION_TYPE, NotificationInterval.DAILY);
+        assertTrue(notificationSettings.isNotificationTypeEnabled(SAMPLE_NOTIFICATION_TYPE));
+    }
+}
