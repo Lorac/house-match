082517f5c660027c8a32ccb60ddc7f77e952c2e8
VampiricRambo <dkatanari@gmail.com>
2015-12-03 11:35:29 -0500

    Complétion de la classe picture.

--- housematch-core/src/main/java/ca/ulaval/glo4003/housematch/domain/picture/Picture.java
+++ housematch-core/src/main/java/ca/ulaval/glo4003/housematch/domain/picture/Picture.java
@@ -2,4 +2,10 @@ package ca.ulaval.glo4003.housematch.domain.picture;
 
 public class Picture {
     private String pathToPicture; //TODO : see if we store a String type
+    private PictureStatus status;
+    
+    public Picture(String pathToPicture, PictureStatus status) {
+        this.pathToPicture = pathToPicture;
+        this.status = status;
+    }
 }
